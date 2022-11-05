74821d7c3ee7f5074b1ff863592cb8c6e9eaa55e
Darcy Murphy <darcy.murphy@loudbit.com>
2022-11-05 13:40:47 -0500

    Create mrdarcymurphy.plugin.zsh

---
+++ mrdarcymurphy.plugin.zsh
@@ -0,0 +1,12 @@
+# prompt
+
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_CHAR_SYMBOL="›"
+SPACESHIP_CHAR_SUFFIX=" "
+
+# aliases
+
+alias ls='ls -AFhgGo'
+alias g=git
+alias gap='git add -p'
+alias gphm='git push heroku master'
